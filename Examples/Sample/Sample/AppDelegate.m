cf7609f23bfb8cc76df5701477677f24ab9354bd
Alex Corcoran <(null)>
2014-11-02 15:03:43 -0600

    Making the sample a storyboard project

--- Examples/Sample/Sample/AppDelegate.m
+++ Examples/Sample/Sample/AppDelegate.m
@@ -13,15 +13,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    ViewController *viewController = [[ViewController alloc] initWithStyle:UITableViewStyleGrouped];
-    
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
-    self.window.rootViewController = navigationController;
-    
-    [self.window makeKeyAndVisible];
-
     return YES;
 }
 
